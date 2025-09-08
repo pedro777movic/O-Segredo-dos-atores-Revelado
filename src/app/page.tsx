9755e5808de6a99f85e4f94ec000e586ddde3e70
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-09-08 23:02:18 +0000

    Descobrir Segredo! (_for element a_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
               className="w-full max-w-md rounded-full text-lg font-bold shadow-lg shadow-primary/30"
               asChild
             >
-              <a href="#pricing">Descobrir Ebook</a>
+              <a href="#pricing">Descobrir Segredo!</a>
             </Button>
             <p className="text-sm font-semibold text-primary">
               Não ficará disponível por muito tempo
