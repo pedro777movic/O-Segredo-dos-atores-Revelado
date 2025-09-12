786e23f65f813ffc3c159ba50d4daee57e7352ad
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-09-12 00:33:55 +0000

    Agora adicione também esse script de captura de UTM nessa página de vend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,14 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="utmify-utms"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        />
       </head>
       <body
         className={cn(
