6fff9dc827097f32345f2bf091e753b320a7e640
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-09-17 02:11:45 +0000

    Remova da VSL os ícone de "assistir mais tarde", "compartilhar", data, l

--- src/components/landing/VSL.tsx
+++ src/components/landing/VSL.tsx
@@ -6,7 +6,7 @@ export function VSL() {
           <div className="aspect-video w-full">
             <iframe
               className="absolute left-0 top-0 h-full w-full"
-              src="https://www.youtube.com/embed/3CZI25GBJF4"
+              src="https://www.youtube.com/embed/3CZI25GBJF4?controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
