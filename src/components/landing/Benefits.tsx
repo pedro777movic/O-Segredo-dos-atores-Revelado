80b168c84791b6712f7f15a2147e81fd49ff3a9c
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-09-19 20:38:51 +0000

    Reescreva aqui: "Eliminar a ansiedade de desempenho e entrar com confian

--- src/components/landing/Benefits.tsx
+++ src/components/landing/Benefits.tsx
@@ -14,7 +14,7 @@ const benefits = [
   },
   {
     icon: Leaf,
-    text: 'Eliminar a ansiedade de desempenho e entrar ccom confiança em qualquer relação.',
+    text: 'Eliminar a ansiedade de desempenho e entrar com confiança em qualquer relação.',
   },
   {
     icon: Ban,
