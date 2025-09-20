81a47264c08c19668c0429fa9a68d58baee8bc5b
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-09-20 02:57:07 +0000

    André F., 27 anos, Paraty/RJ (_for element p_)

--- src/components/landing/SocialProof.tsx
+++ src/components/landing/SocialProof.tsx
@@ -12,7 +12,7 @@ const testimonials = [
   {
     quote:
       'Foi o santo remédio. Fui de uns 5 min para mais de 1 hora fácil, e só paro agora quando a gata goza 😈',
-    name: 'André F.',
+    name: 'André F., 27 anos, Paraty/RJ',
     avatar: 'https://picsum.photos/seed/men0/100/100',
     'data-ai-hint': 'man portrait'
   },
