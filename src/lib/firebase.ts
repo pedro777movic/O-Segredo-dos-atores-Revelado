3fe71a66ba88e9047b9e73920c4e1cfd0e8b28aa
Pedro Victor Barbosa <pedromovic2017@gmail.com>
2025-09-21 01:25:17 +0000

    Ajustar firebaseConfig.js (se ainda não fez):
    código:
    
    import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,6 +12,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 export { app };
